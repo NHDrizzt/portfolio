4d376aa362394acb35889da9acf01d6627c6beed
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-03-01 22:58:38 -0300

    footer component

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,8 +1,21 @@
 import './footer.css'
+import githubIcon from '../assets/images/icon-github.svg'
+import frontEndIcon from '../assets/images/icon-frontend-mentor.svg'
+import linkedinIcon from '../assets/images/icon-linkedin.svg'
+import twitterIcon from '../assets/images/icon-twitter.svg'
+
 function Footer() {
     return (
         <footer className='footer'>
-            nhdrizzt
+            <h1>nhdrizzt</h1>
+            <nav>
+                <div className="nav-footer-container">
+                    <img src={githubIcon} alt="github-icon"/>
+                    <img src={frontEndIcon} alt="github-icon"/>
+                    <img src={linkedinIcon} alt="github-icon"/>
+                    <img src={twitterIcon} alt="github-icon"/>
+                </div>
+            </nav>
         </footer>
     )
 }
