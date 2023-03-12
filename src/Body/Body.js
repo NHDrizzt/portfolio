1f507180b46a2ee50d2f3925c6a8214fd04b30d1
NHDrizzt <biel_s.carvalho2@hotmail.com>
2023-03-12 14:30:21 -0300

    my projects section

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -10,6 +10,13 @@ import profilePicture1200 from "../assets/images/image-profile-desktop.webp";
 import profilePicture600 from "../assets/images/image-profile-tablet.webp";
 import profilePicture375 from "../assets/images/image-profile-mobile.webp";
 import circleImage from "../assets/images/pattern-circle.svg";
+import projectPictureOneLarge from "../assets/images/thumbnail-project-1-large.webp";
+import projectPictureTwoLarge from "../assets/images/thumbnail-project-2-large.webp";
+import projectPictureThreeLarge from "../assets/images/thumbnail-project-3-large.webp";
+import projectPictureFourLarge from "../assets/images/thumbnail-project-4-large.webp";
+import projectPictureFiveLarge from "../assets/images/thumbnail-project-5-large.webp";
+import projectPictureSixLarge from "../assets/images/thumbnail-project-6-large.webp";
+
 
 function Body() {
     return (
@@ -68,96 +75,138 @@ function Body() {
                 </div>
             </article>
             <article className='my-projects'>
-                <div className="my-projects-header">
-                    <h1>Projects</h1>
-                    <h3>Contact me</h3>
-                </div>
-                <div className="project">
-                    <picture>
-                        <img src={projectOne} alt="project-one"/>
-                    </picture>
-                    <h2>Design portfolio</h2>
-                    <div className="project-title-tec">
-                        <p>HTML</p>
-                        <p>CSS</p>
+                <div className="wrapper">
+                    <div className="my-projects-header">
+                        <h1>Projects</h1>
+                        <h3>Contact me</h3>
                     </div>
-                    <div className="project-view-code">
-                        <h3>View project</h3>
-                        <h3>View code</h3>
-                    </div>
-                </div>
-                <div className="project">
-                    <picture>
-                        <img src={projectTwo} alt=""/>
-                    </picture>
-                    <h2>E-learning landing page</h2>
-                    <div className="project-title-tec">
-                        <p>HTML</p>
-                        <p>CSS</p>
-                    </div>
-                    <div className="project-view-code">
-                        <h3>View project</h3>
-                        <h3>View code</h3>
-                    </div>
-                </div>
-                <div className="project">
-                    <picture>
-                        <img src={projectThree} alt=""/>
-                    </picture>
-                    <h2>Todo web app</h2>
-                    <div className="project-title-tec">
-                        <p>HTML</p>
-                        <p>CSS</p>
-                        <p>JavaScript</p>
-                    </div>
-                    <div className="project-view-code">
-                        <h3>View project</h3>
-                        <h3>View code</h3>
-                    </div>
-                </div>
-                <div className="project">
-                    <picture>
-                        <img src={projectFour} alt=""/>
-                    </picture>
-                    <h2>Entertainment web app</h2>
-                    <div className="project-title-tec">
-                        <p>HTML</p>
-                        <p>CSS</p>
-                        <p>JavaScript</p>
-                    </div>
-                    <div className="project-view-code">
-                        <h3>View project</h3>
-                        <h3>View code</h3>
-                    </div>
-                </div>
-                <div className="project">
-                    <picture>
-                        <img src={projectFive} alt=""/>
-                    </picture>
-                    <h2>Memory Game</h2>
-                    <div className="project-title-tec">
-                        <p>HTML</p>
-                        <p>CSS</p>
-                        <p>JavaScript</p>
-                    </div>
-                    <div className="project-view-code">
-                        <h3>View project</h3>
-                        <h3>View code</h3>
-                    </div>
-                </div>
-                <div className="project">
-                    <picture>
-                        <img src={projectSix} alt=""/>
-                    </picture>
-                    <h2>Art gallery showcase</h2>
-                    <div className="project-title-tec">
-                        <p>HTML</p>
-                        <p>CSS</p>
-                        <p>JavaScript</p>
-                    </div>
-                    <div className="project-view-code">
-                        <h3>View project</h3>
-                        <h3>View code</h3>
+                    <div className="grid-container">
+                        <div className="project">
+                            <picture>
+                                <source
+                                    media="(min-width: 62.5em)"
+                                    srcSet={projectPictureOneLarge}
+                                    width="540px"
+                                    height="400px"
+                                />
+                                <img
+                                    src={projectOne}
+                                    alt="project-one"/>
+                            </picture>
+                            <h2>Design portfolio</h2>
+                            <div className="project-title-tec">
+                                <p>HTML</p>
+                                <p>CSS</p>
+                            </div>
+                            <div className="project-view-code">
+                                <h3>View project</h3>
+                                <h3>View code</h3>
+                            </div>
+                        </div>
+                        <div className="project">
+                            <picture>
+                                <source
+                                    media="(min-width: 62.5em)"
+                                    srcSet={projectPictureTwoLarge}
+                                    width="540px"
+                                    height="400px"
+                                />
+                                <img src={projectTwo} alt=""/>
+                            </picture>
+                            <h2>E-learning landing page</h2>
+                            <div className="project-title-tec">
+                                <p>HTML</p>
+                                <p>CSS</p>
+                            </div>
+                            <div className="project-view-code">
+                                <h3>View project</h3>
+                                <h3>View code</h3>
+                            </div>
+                        </div>
+                        <div className="project">
+                            <picture>
+                                <source
+                                    media="(min-width: 62.5em)"
+                                    srcSet={projectPictureThreeLarge}
+                                    width="540px"
+                                    height="400px"
+                                />
+                                <img src={projectThree} alt=""/>
+                            </picture>
+                            <h2>Todo web app</h2>
+                            <div className="project-title-tec">
+                                <p>HTML</p>
+                                <p>CSS</p>
+                                <p>JavaScript</p>
+                            </div>
+                            <div className="project-view-code">
+                                <h3>View project</h3>
+                                <h3>View code</h3>
+                            </div>
+                        </div>
+                        <div className="project">
+                            <picture>
+                                <source
+                                    media="(min-width: 62.5em)"
+                                    srcSet={projectPictureFourLarge}
+                                    width="540px"
+                                    height="400px"
+                                />
+                                <img src={projectFour} alt=""/>
+                            </picture>
+                            <h2>Entertainment web app</h2>
+                            <div className="project-title-tec">
+                                <p>HTML</p>
+                                <p>CSS</p>
+                                <p>JavaScript</p>
+                            </div>
+                            <div className="project-view-code">
+                                <h3>View project</h3>
+                                <h3>View code</h3>
+                            </div>
+                        </div>
+                        <div className="project">
+                            <picture>
+                                <source
+                                    media="(min-width: 62.5em)"
+                                    srcSet={projectPictureFiveLarge}
+                                    width="540px"
+                                    height="400px"
+                                />
+                                <img src={projectFive} alt=""/>
+                            </picture>
+                            <h2>Memory Game</h2>
+                            <div className="project-title-tec">
+                                <p>HTML</p>
+                                <p>CSS</p>
+                                <p>JavaScript</p>
+                            </div>
+                            <div className="project-view-code">
+                                <h3>View project</h3>
+                                <h3>View code</h3>
+                            </div>
+                        </div>
+                        <div className="project">
+                            <picture>
+                                <source
+                                    media="(min-width: 62.5em)"
+                                    srcSet={projectPictureSixLarge}
+                                    width="540px"
+                                    height="400px"
+                                />
+                                <img src={projectSix} alt=""/>
+                            </picture>
+                            <h2>Art gallery showcase</h2>
+                            <div className="project-title-tec">
+                                <p>HTML</p>
+                                <p>CSS</p>
+                                <p>JavaScript</p>
+                            </div>
+                            <div className="project-view-code">
+                                <h3>View project</h3>
+                                <h3>View code</h3>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </article>
